5a7efd57b8dcba4e3112d9d3de1adddb8d7b8ce2
woahdas <78304648+woahdas@users.noreply.github.com>
2021-03-03 17:39:43 +0000

    Create ThirdAI.py

---
+++ ThirdAI.py
@@ -0,0 +1,33 @@
+#This is the best one so far, and scored 0.76 (76% accurate) on Kaggle!
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+train = pd.read_csv('/kaggle/input/titanic/train.csv')
+test = pd.read_csv('/kaggle/input/titanic/test.csv')
+
+
+train = train.drop(['Name','SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],axis=1)
+test = test.drop(['Name','SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],axis=1)
+for df in [train,test]:
+        df['Sex_binary']=df['Sex'].map({'male':1,'female':0})
+    
+
+train['Age'] = train['Age'].fillna(0)
+test['Age'] = test['Age'].fillna(0)
+
+features = ['Pclass','Age','Sex_binary']
+target = 'Survived'
+
+###
+###
+
+logreg = LogisticRegression()
+
+logreg.fit(train[features],train[target])
+
+logreg_predictions = logreg.predict(test[features])
+#logreg_data = pd.read_csv("/kaggle/input/titanic/test.csv")
+
+submission = pd.DataFrame({'PassengerId':test['PassengerId'],'Survived':logreg_predictions})
+submission.to_csv('submission2.csv', index=False)
